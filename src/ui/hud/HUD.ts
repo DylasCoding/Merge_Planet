978da9526d59f20e89316f0bb39b3a3a7ff977ad
DylasCoding <hieusih@gmail.com>
2026-07-09 22:44:40 +0700

    feat: add empty TopBar to HUD

--- src/ui/hud/HUD.ts
+++ src/ui/hud/HUD.ts
@@ -1,7 +1,12 @@
 import * as PIXI from "pixi.js";
+import { TopBar } from "./TopBar";
 
 export class HUD extends PIXI.Container {
+    private topBar: TopBar;
+
     constructor() {
         super();
+        this.topBar = new TopBar();
+        this.addChild(this.topBar);
     }
 }
---
+++ src/ui/hud/TopBar.ts
@@ -0,0 +1,7 @@
+import * as PIXI from "pixi.js";
+
+export class TopBar extends PIXI.Container {
+    constructor() {
+        super();
+    }
+}
