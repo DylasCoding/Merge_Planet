84c9c0fe2180621a0bc6f48b5537106edaee2e96
DylasCoding <hieusih@gmail.com>
2026-07-09 22:50:55 +0700

    feat: add empty BottomProgress to HUD

--- src/ui/hud/HUD.ts
+++ src/ui/hud/HUD.ts
@@ -1,12 +1,17 @@
 import * as PIXI from "pixi.js";
 import { TopBar } from "./TopBar";
+import { BottomProgress } from "./BottomProgress";
 
 export class HUD extends PIXI.Container {
     private topBar: TopBar;
+    private bottomProgress: BottomProgress;
 
     constructor() {
         super();
         this.topBar = new TopBar();
+        this.bottomProgress = new BottomProgress();
+
         this.addChild(this.topBar);
+        this.addChild(this.bottomProgress);
     }
 }
