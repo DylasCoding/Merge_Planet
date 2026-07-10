e00dcf9903958fa4c6aeaee733670303558b5b2b
DylasCoding <hieusih@gmail.com>
2026-07-10 10:37:52 +0700

    feat: add score display to Topbar

--- src/ui/hud/TopBar.ts
+++ src/ui/hud/TopBar.ts
@@ -1,7 +1,14 @@
 import * as PIXI from "pixi.js";
+import { ScoreView } from "../components/ScoreView.ts";
 
 export class TopBar extends PIXI.Container {
+    private score: ScoreView;
+
     constructor() {
         super();
+
+        this.score = new ScoreView();
+        this.score.setScoreText(12);
+        this.addChild(this.score);
     }
 }
