3fb742917d5fb92939e9b31fe0de9f551c96c284
DylasCoding <hieusih@gmail.com>
2026-07-10 11:53:18 +0700

    refactor: store app instance in BaseScene for UI scaling

--- src/scenes/BaseScene.ts
+++ src/scenes/BaseScene.ts
@@ -1,6 +1,13 @@
-import { Container } from "pixi.js";
+import { type Application, Container } from "pixi.js";
 
 export abstract class BaseScene extends Container {
+    protected readonly app: Application;
+
+    constructor(app: Application) {
+        super();
+        this.app = app;
+    }
+
     public abstract initialize(): void;
 
     public update(delta: number): void {}
