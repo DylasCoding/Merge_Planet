c455f3ffcee639f3225936fb01e3fae0aa892ba3
DylasCoding <hieusih@gmail.com>
2026-07-10 11:51:40 +0700

    refactor: disable scene scrolling

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
-import { Application, Assets } from "pixi.js";
+import { Application, Assets, Sprite } from "pixi.js";
 import { Game } from "./Game";
 import { manifest } from "./core/manifest.ts";
+import "./style.css";
 
 async function main() {
     const app = new Application();
 
     await app.init({
         resizeTo: window,
-        background: "#1b1b1b",
+        background: "#fdfdfd",
     });
 
     await Assets.init({ manifest });
+    await Assets.loadBundle("backgrounds");
+    const background = Sprite.from("background_sky");
+
+    function resizeBackground() {
+        background.width = app.screen.width;
+        background.height = app.screen.height;
+    }
+
+    resizeBackground();
+
+    app.renderer.on("resize", resizeBackground);
+
+    app.stage.addChild(background);
 
     document.body.appendChild(app.canvas);
 
