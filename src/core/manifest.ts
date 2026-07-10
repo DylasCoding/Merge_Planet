cedd2cdabf9b8172f259ff326987ea1e856d8c00
DylasCoding <hieusih@gmail.com>
2026-07-10 14:04:07 +0700

    feat: add TTF font and Font.ts manager

--- src/core/manifest.ts
+++ src/core/manifest.ts
@@ -13,6 +13,9 @@ import score_bg from "../assets/Containers/Small/pause-container-small.png";
 import gem_bg from "../assets/Rank_System_Components/Medium/ranking-highlight-medium.png";
 import gem_icon from "../assets/Icons/gem-1-64.png";
 
+import Asap_Bold from "../assets/Fonts/Asap-Bold.ttf";
+import Righteous_Regular from "../assets/Fonts/Righteous-Regular.ttf";
+
 export const manifest: AssetsManifest = {
     bundles: [
         {
@@ -39,6 +42,13 @@ export const manifest: AssetsManifest = {
         {
             name: "planets",
             assets: {},
+        },
+        {
+            name: "fonts",
+            assets: {
+                Asap_Bold: Asap_Bold,
+                Righteous_Regular: Righteous_Regular,
+            },
         },
     ],
 };
