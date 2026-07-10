2daf368179af0eed8afa6e0449e3321c1a8d42c5
DylasCoding <hieusih@gmail.com>
2026-07-10 15:36:10 +0700

    feat: apply GameBox display to GameScene

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,17 +1,23 @@
 import { BaseScene } from "./BaseScene.ts";
 import { type Application, Assets, Container } from "pixi.js";
 import { HUD } from "../ui/hud/HUD.ts";
+import { GameBox } from "../ui/components/GameBox.ts";
 
 export class GameScene extends BaseScene {
     private readonly world = new Container();
     private hud!: HUD;
+    private gameBox!: GameBox;
 
     constructor(app: Application) {
         super(app);
     }
 
     public async initialize(): Promise<void> {
-        await Assets.loadBundle(["ui", "backgrounds"]);
+        await Assets.loadBundle(["ui"]);
+
+        this.gameBox = new GameBox();
+        this.gameBox.position.set(this.app.screen.width / 2, this.app.screen.height / 2);
+        this.world.addChild(this.gameBox);
 
         this.hud = new HUD(this.app);
 
