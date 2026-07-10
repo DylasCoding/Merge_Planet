27eba2d914b0a1baaabab34308d22866cb835637
Tunaanhgamedev <shelockhomles2005@gmail.com>
2026-07-10 11:00:27 +0700

    feat: implement planet data model

---
+++ src/features/planet/data/PlanetConfig.ts
@@ -0,0 +1,73 @@
+import { type PlanetData } from "./PlanetData";
+
+export const PLANET_CONFIG: Record<number, PlanetData> = {
+    1: {
+        level: 1,
+        radius: 15,
+        mass: 1,
+        textureKey: "planet1",
+    },
+
+    2: {
+        level: 2,
+        radius: 22,
+        mass: 2,
+        textureKey: "planet2",
+    },
+
+    3: {
+        level: 3,
+        radius: 29,
+        mass: 3,
+        textureKey: "planet3",
+    },
+
+    4: {
+        level: 4,
+        radius: 36,
+        mass: 4,
+        textureKey: "planet4",
+    },
+
+    5: {
+        level: 5,
+        radius: 43,
+        mass: 5,
+        textureKey: "planet5",
+    },
+
+    6: {
+        level: 6,
+        radius: 50,
+        mass: 6,
+        textureKey: "planet6",
+    },
+
+    7: {
+        level: 7,
+        radius: 57,
+        mass: 7,
+        textureKey: "planet7",
+    },
+
+    8: {
+        level: 8,
+        radius: 64,
+        mass: 8,
+        textureKey: "planet8",
+    },
+
+    9: {
+        level: 9,
+        radius: 71,
+        mass: 9,
+        textureKey: "planet9",
+    },
+
+    10: {
+        level: 10,
+        radius: 88,
+        mass: 10,
+        textureKey: "planet10",
+    },
+};
