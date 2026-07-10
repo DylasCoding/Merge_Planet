d23d660dbc38081fb7eae861c1aadfd58b3814c6
DylasCoding <hieusih@gmail.com>
2026-07-10 09:38:55 +0700

    feat: Add Game bootstrap class

---
+++ src/Game.ts
@@ -0,0 +1,22 @@
+import { Application } from "pixi.js";
+import { GameScene } from "./scenes/GameScene.ts";
+
+export class Game {
+    private readonly app: Application;
+    private readonly gameScene: GameScene;
+
+    constructor(app: Application) {
+        this.app = app;
+        this.gameScene = new GameScene();
+    }
+
+    public async initialize(): Promise<void> {
+        await this.gameScene.initialize();
+
+        this.app.stage.addChild(this.gameScene);
+
+        this.app.ticker.add(() => {
+            this.gameScene.update();
+        });
+    }
+}
