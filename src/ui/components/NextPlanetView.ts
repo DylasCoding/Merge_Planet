768ca3b2b2660f5ad4d963a3df5c1c448e698def
DylasCoding <hieusih@gmail.com>
2026-07-10 17:02:19 +0700

    feat: NextPlanetView change icon when using setPlanetIcon

--- src/ui/components/NextPlanetView.ts
+++ src/ui/components/NextPlanetView.ts
@@ -2,6 +2,8 @@ import { type Application, Container, Sprite, Text } from "pixi.js";
 import { Font } from "../../core/Font.ts";
 
 export class NextPlanetView extends Container {
+    private icon: Sprite;
+
     constructor() {
         super();
 
@@ -9,26 +11,30 @@ export class NextPlanetView extends Container {
             text: "NEXT PLANET",
             style: {
                 fontFamily: Font.Asap_Bold,
-                fill: 0xffffff,
+                fill: 0xffffb3,
                 fontSize: 16,
             },
         });
 
         const bg = Sprite.from("nextPlanet_bg");
-        const icon = Sprite.from("planet1");
+        this.icon = Sprite.from("planet1");
 
         bg.scale.x = 0.45;
         bg.scale.y = 0.5;
-        icon.scale.set(0.5);
+        this.icon.scale.set(0.5);
 
         text.anchor.set(0.5, 1);
         bg.anchor.set(0.5, 0);
-        icon.anchor.set(0.5);
+        this.icon.anchor.set(0.5);
 
         bg.position.set(0, 30);
-        icon.position.set(bg.x, bg.y + bg.height * 0.5);
+        this.icon.position.set(bg.x, bg.y + bg.height * 0.5);
         text.position.set(bg.x, bg.y - 8);
 
-        this.addChild(bg, icon, text);
+        this.addChild(bg, this.icon, text);
+    }
+
+    public setPlanetIcon(planetTexture: Sprite): void {
+        this.icon.texture = planetTexture.texture;
     }
 }
--- src/ui/hud/TopBar.ts
+++ src/ui/hud/TopBar.ts
@@ -1,13 +1,15 @@
 import * as PIXI from "pixi.js";
 import { ScoreView } from "../components/ScoreView.ts";
 import { GemView } from "../components/GemView.ts";
-import { Container, Application } from "pixi.js";
+import { Container, Application, Sprite } from "pixi.js";
 import { NextPlanetView } from "../components/NextPlanetView.ts";
+import { Button } from "../components/Button.ts";
 
 export class TopBar extends PIXI.Container {
     private score: ScoreView;
     private gem: GemView;
     private nextPlanet: NextPlanetView;
+    private skinButton: Button;
 
     private readonly leftArea = new Container();
     private readonly rightArea = new Container();
@@ -20,19 +22,26 @@ export class TopBar extends PIXI.Container {
         this.gem.setGemText(5000);
 
         this.score = new ScoreView();
-        this.score.setScoreText(12);
+        this.score.setScoreText(120);
 
         this.nextPlanet = new NextPlanetView();
+        this.nextPlanet.setPlanetIcon(Sprite.from("planet2"));
+
+        this.skinButton = new Button("SKIN", Sprite.from("button_yellow"));
+        this.skinButton.onClick(() => {
+            console.log("abc");
+        });
 
         this.leftArea.position.set(20, 20);
-
         this.centerArea.position.set(app.screen.width / 2, 20);
-
         this.rightArea.position.set(app.screen.width - 100, 20);
+
+        this.nextPlanet.position.set(0, 0);
+        this.skinButton.position.set(0, 130);
 
         this.leftArea.addChild(this.gem);
         this.centerArea.addChild(this.score);
-        this.rightArea.addChild(this.nextPlanet);
+        this.rightArea.addChild(this.nextPlanet, this.skinButton);
 
         this.addChild(this.leftArea, this.centerArea, this.rightArea);
     }
