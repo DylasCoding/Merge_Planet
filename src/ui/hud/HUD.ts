a3fa8a54b591165f889c48f497f1adc03033ba63
DylasCoding <hieusih@gmail.com>
2026-07-10 23:32:21 +0700

    refactor: move planet levels sidebar into HUD component

--- src/ui/hud/HUD.ts
+++ src/ui/hud/HUD.ts
@@ -1,18 +1,22 @@
 import * as PIXI from "pixi.js";
 import { TopBar } from "./TopBar";
 import { BottomProgress } from "./BottomProgress";
-import type { Application } from "pixi.js";
+import { Application } from "pixi.js";
+import { LeftSideBar } from "./LeftSideBar.ts";
 
 export class HUD extends PIXI.Container {
     private topBar: TopBar;
     private bottomProgress: BottomProgress;
+    private leftSideBar: LeftSideBar;
 
     constructor(app: Application) {
         super();
         this.topBar = new TopBar(app);
         this.bottomProgress = new BottomProgress();
+        this.leftSideBar = new LeftSideBar(app);
 
         this.addChild(this.topBar);
         this.addChild(this.bottomProgress);
+        this.addChild(this.leftSideBar);
     }
 }
