db18160b3d0c5e9208a90fbc83865f17deac810f
Tunaanhgamedev <shelockhomles2005@gmail.com>
2026-07-10 15:57:45 +0700

    refactor: make Planet extend Container

--- src/features/planet/entities/Planet.ts
+++ src/features/planet/entities/Planet.ts
@@ -1,39 +1,26 @@
-import { Sprite, Texture } from "pixi.js";
+import { Container, Sprite, Texture } from "pixi.js";
 import { Vector2 } from "../../../utils/math/Vector2";
 import { type PlanetData } from "../data/PlanetData";
 import { type PlanetOptions } from "../types/PlanetOptions";
 
-export class Planet {
+export class Planet extends Container {
     public readonly data: PlanetData;
     public readonly sprite: Sprite;
 
-    public position: Vector2;
     public velocity: Vector2;
 
     constructor(options: PlanetOptions) {
+        super();
         const { data, sprite, position = new Vector2(), velocity = new Vector2() } = options;
         this.data = data;
         this.sprite = sprite;
 
         this.position = position;
         this.velocity = velocity;
-
-        this.syncSpritePosition();
-    }
-
-    private syncSpritePosition(): void {
-        this.sprite.position.set(this.position.x, this.position.y);
     }
 
     public setTexture(texture: Texture): void {
         this.sprite.texture = texture;
-    }
-
-    public setPosition(x: number, y: number): void {
-        this.position.x = x;
-        this.position.y = y;
-
-        this.syncSpritePosition();
     }
 
     public setVelocity(vx: number, vy: number): void {
@@ -43,8 +30,7 @@ export class Planet {
 
     public destroy(): void {
         this.sprite.destroy({
-            texture: false,
-            textureSource: false,
+            children: true,
         });
     }
 }
