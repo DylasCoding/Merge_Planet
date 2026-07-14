c36189ec8d91021f7f764f761105af234a51e3be
Tunaanhgamedev <shelockhomles2005@gmail.com>
2026-07-14 23:00:04 +0700

    feat: add event onPickaxeClicked

--- src/ui/hud/HUD.ts
+++ src/ui/hud/HUD.ts
@@ -9,14 +9,14 @@ export class HUD extends Container {
     private topBar: TopBar;
     private bottomProgress: BottomProgress;
     private leftSideBar: LeftSideBar;
-    private rightSideBar: RightSideBar;
+    public rightSideBar: RightSideBar;
 
-    constructor(app: Application, onOpenSettings: () => void, onOpenSkinShop: () => void) {
+    constructor(app: Application, onOpenSettings: () => void, onOpenSkinShop: () => void, onPickaxeClick: () => void) {
         super();
         this.topBar = new TopBar(app);
         this.bottomProgress = new BottomProgress();
         this.leftSideBar = new LeftSideBar(app);
-        this.rightSideBar = new RightSideBar(app);
+        this.rightSideBar = new RightSideBar(app, onPickaxeClick);
 
         this.leftSideBar.pointToPlanet(2);
         this.topBar.settingsButton.onClick(onOpenSettings);
--- src/ui/hud/RightSideBar.ts
+++ src/ui/hud/RightSideBar.ts
@@ -4,29 +4,34 @@ import { ToolButton } from "../components/ToolButton.ts";
 import { Button } from "../components/Button.ts";
 
 export class RightSideBar extends Container {
-    constructor(app: Application) {
+    public pickaxeButton: ToolButton;
+    constructor(app: Application, onPickaxeClick: () => void,) {
         super();
 
         this.x = app.screen.width;
         this.y = 0;
 
-        const bg = Sprite.from("right_sidebar_bg");
+        const bg = new Sprite();
         bg.anchor.set(1, 0);
         bg.height = app.screen.height;
 
         const toolsContainer = new Container();
 
-        const tool1 = new ToolButton("tool1_icon", 100, () => {});
+        this.pickaxeButton = new ToolButton("tool1_icon", 100, onPickaxeClick);
         const tool2 = new ToolButton("tool2_icon", 350, () => {});
 
         const toolSpacing = 100;
-        tool1.y = 0;
-        tool2.y = tool1.y + toolSpacing;
+        this.pickaxeButton.y = 0;
+        tool2.y = this.pickaxeButton.y + toolSpacing;
 
-        toolsContainer.addChild(tool1, tool2);
+        toolsContainer.addChild(this.pickaxeButton, tool2);
         toolsContainer.x = -bg.width / 2 - 50;
         toolsContainer.y = app.screen.height - toolsContainer.height;
 
         this.addChild(bg, toolsContainer);
+        
+        this.pickaxeButton.on("pointertap", () => {
+            this.emit("Pickaxe");
+        });
     }
 }
