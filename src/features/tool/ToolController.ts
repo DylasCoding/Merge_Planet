244f98a61cc862f8216c9024d2c04472a703d887
Tunaanhgamedev <shelockhomles2005@gmail.com>
2026-07-14 16:35:10 +0700

    feat: add overlay effect for tool controller

---
+++ src/features/tool/ToolController.ts
@@ -0,0 +1,49 @@
+import { Planet } from "../planet/entities/Planet";
+import { ToolManager } from "./ToolManager";
+import { ToolType } from "./ToolType";
+import { PickaxeTool } from "./tools/PickaxeTool";
+
+export class ToolController {
+    private readonly toolManager: ToolManager;
+    private readonly pickaxeTool: PickaxeTool;
+
+    constructor(toolManager: ToolManager, pickaxeTool: PickaxeTool) {
+        this.toolManager = toolManager;
+        this.pickaxeTool = pickaxeTool;
+    }
+
+    public selectTool(tool: ToolType): void {
+        this.toolManager.toggle(tool);
+    }
+
+    public cancelTool(): void {
+        this.toolManager.cancel();
+    }
+
+    public onPlanetClicked(planet: Planet): void {
+        if (!this.toolManager.isUsingTool()) {
+            return;
+        }
+
+        switch (this.toolManager.getCurrentTool()) {
+            case ToolType.Pickaxe:
+                this.pickaxeTool.use(planet);
+                break;
+
+            case ToolType.None:
+            default:
+                return;
+        }
+
+        this.toolManager.cancel();
+    }
+
+    public isUsingTool(): boolean {
+        return this.toolManager.isUsingTool();
+    }
+
+    public toggleTool(tool: ToolType): void {
+        console.log("Toggle:", tool);
+        this.toolManager.toggle(tool);
+    }
+}
