b47dfe6648cc0ce6079d74bc06a378087095b389
Tunaanhgamedev <shelockhomles2005@gmail.com>
2026-07-16 15:12:10 +0700

    feat: add button event shuffle

--- src/ui/hud/HUD.ts
+++ src/ui/hud/HUD.ts
@@ -9,11 +9,11 @@ export class HUD extends Container {
     private leftSideBar: LeftSideBar;
     public rightSideBar: RightSideBar;
 
-    constructor(app: Application, onOpenSettings: () => void, onOpenSkinShop: () => void, onPickaxeClick: () => void) {
+    constructor(app: Application, onOpenSettings: () => void, onOpenSkinShop: () => void, onPickaxeClick: () => void, onShuffleClick: () => void) {
         super();
         this.topBar = new TopBar(app);
         this.leftSideBar = new LeftSideBar(app);
-        this.rightSideBar = new RightSideBar(app, onPickaxeClick);
+        this.rightSideBar = new RightSideBar(app, onPickaxeClick, onShuffleClick);
 
         this.leftSideBar.pointToPlanet(2);
         this.topBar.settingsButton.onClick(onOpenSettings);
