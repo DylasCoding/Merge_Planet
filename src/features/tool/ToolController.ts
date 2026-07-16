1660db5250bb02f283e9cda9ae70e787ea2fe666
Tunaanhgamedev <shelockhomles2005@gmail.com>
2026-07-16 16:10:21 +0700

    feat: integrate pickaxe cursor with tool controller

--- src/features/tool/ToolController.ts
+++ src/features/tool/ToolController.ts
@@ -1,6 +1,7 @@
 import { Planet } from "../planet/entities/Planet";
 import { ToolManager } from "./ToolManager";
 import { ToolType } from "./ToolType";
+import type { PickaxeCursor } from "./effects/PickaxeCursor";
 import { PickaxeEffect } from "./effects/PickaxeEffect";
 import type { ShakeBoxEffect } from "./effects/ShakeBoxEffect";
 import { PickaxeTool } from "./tools/PickaxeTool";
@@ -10,6 +11,7 @@ export class ToolController {
     private readonly toolManager: ToolManager;
     private readonly pickaxeTool: PickaxeTool;
     private readonly pickaxeEffect: PickaxeEffect;
+    private readonly pickaxeCursor: PickaxeCursor;
     private readonly shuffleTool: ShuffleTool;
     private readonly shakeBoxEffect: ShakeBoxEffect;
 
@@ -17,21 +19,20 @@ export class ToolController {
         toolManager: ToolManager,
         pickaxeTool: PickaxeTool,
         pickaxeEffect: PickaxeEffect,
+        pickaxeCursor: PickaxeCursor,
         shuffleTool: ShuffleTool,
         shakeBoxEffect: ShakeBoxEffect,
     ) {
         this.toolManager = toolManager;
         this.pickaxeTool = pickaxeTool;
         this.pickaxeEffect = pickaxeEffect;
+        this.pickaxeCursor = pickaxeCursor;
         this.shuffleTool = shuffleTool;
         this.shakeBoxEffect = shakeBoxEffect;
     }
 
-    public selectTool(tool: ToolType): void {
-        this.toolManager.toggle(tool);
-    }
-
     public cancelTool(): void {
+        this.pickaxeCursor.hide();
         this.toolManager.cancel();
     }
 
@@ -54,6 +55,7 @@ export class ToolController {
 
         switch (this.toolManager.getCurrentTool()) {
             case ToolType.Pickaxe:
+                this.pickaxeCursor.hide();
                 await this.pickaxeEffect.play(planet);
                 this.pickaxeTool.use(planet);
                 break;
@@ -64,6 +66,7 @@ export class ToolController {
         }
 
         this.toolManager.cancel();
+        this.pickaxeCursor.hide();
 
         this.onToolFinished?.();
     }
@@ -75,5 +78,11 @@ export class ToolController {
     public toggleTool(tool: ToolType): void {
         console.log("Toggle:", tool);
         this.toolManager.toggle(tool);
+
+        if (this.toolManager.isSelected(ToolType.Pickaxe)) {
+            this.pickaxeCursor.show();
+        } else {
+            this.pickaxeCursor.hide();
+        }
     }
 }
