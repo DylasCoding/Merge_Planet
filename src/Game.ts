39bb16e6823ceefba962c327de3ccdea81a04af3
DylasCoding <hieusih@gmail.com>
2026-07-16 14:31:05 +0700

    fix: memory leak when reload game

--- src/Game.ts
+++ src/Game.ts
@@ -1,6 +1,7 @@
 import { Application } from "pixi.js";
 import { GameScene } from "./scenes/GameScene.ts";
 import { EventBus, GameEvent } from "./core/event/GameEvent.ts";
+import { GameSession } from "./core/manager/GameSession.ts";
 
 export class Game {
     private readonly app: Application;
@@ -24,6 +25,11 @@ export class Game {
     }
 
     private async reloadGameScene(): Promise<void> {
+        EventBus.instance.removeAllListeners();
+        EventBus.instance.on(GameEvent.GameStart, this.reloadGameScene, this);
+
+        GameSession.Instance.reset();
+
         if (this.gameScene) {
             this.app.stage.removeChild(this.gameScene);
             this.gameScene.destroy({ children: true });
